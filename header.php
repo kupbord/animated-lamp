a8c75d3dd49d2bb647d6ae86acf91d1913d5692d
RiskDevKit <risk61358@gmail.com>
2022-09-12 14:54:38 -0400

    Update header.php

--- header.php
+++ header.php
@@ -3,8 +3,8 @@
               <small><span style="text-align: right;"><!--Logged in as <b>jko64</b>--> <a href="#" class="navlink">sign in</a> | <a href="#" class="navlink">sign up</a> </span> | <a class="navlink" href=".">home</a> | <a class="navlink" href=".">users</a> | <a class="navlink" href=".">videos</a> | <a class="navlink" href=".">upload</a></small>
               </div>
               <div class="imgheader">
-              <img src="headerimg/retro.png">
-              <!-- homepage logo banner dimensions are 650x200 -->
+              <img src="headerimg/cosmix.png">
+              <!-- homepage logo banner dimensions are 650x80 -->
               </div>
           <!--    <div class="extranav">
               <p>this is extra navigation shit idk if this will stay</p>
