f4a70b83136aafb68aa6b1774416ad43972836ec
RiskDevKit <risk61358@gmail.com>
2022-09-16 15:26:24 -0400

    custom uh range styling

--- playground.php
+++ playground.php
@@ -5,12 +5,27 @@
     <input type="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
     <br>
     <small id="emailHelp">We'll never share your email with anyone else. (This doesn't do anything)</small>
-<br>
-<br>
+    
+    <br>
+    <br>
+    
     <label for="password">Password</label>
     <input type="password" id="password" aria-describedby="passHelp" placeholder="Choose password">
     <br>
     <small id="passHelp">Be sure to pick something secure. (This doesn't do anything)</small>
+
+    <br>
+    <br>
+
+    <label for="exampleFormControlFile1">Upload video file (max 100mb)</label>
+    <br>
+    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+
+    <br>
+    <br>
+
+    <label for="volume">Volume</label>
+    <input type="range" id="volume">
 </form>
 
 <div class="footerl"><small id="footer">&copy;2022 kupbord</small></div>
